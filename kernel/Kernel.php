6decbb32bc293482509f62441022ebc68ee64e48
Leon <mac@macdeiMac.local>
2026-07-22 18:01:54 +0800

    debug: enable error display to diagnose white screen

--- kernel/Kernel.php
+++ kernel/Kernel.php
@@ -12,7 +12,8 @@ use Kernel\Util\Context;
 use Kernel\Util\Plugin;
 use Kernel\Waf\Firewall;
 
-error_reporting(0);
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 const BASE_PATH = __DIR__ . "/../";
 require(BASE_PATH . '/vendor/autoload.php');
 require("Helper.php");
